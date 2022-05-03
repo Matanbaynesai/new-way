cd66d7736095f3b2dce3dc6776e7c5b4f8ee8c07
matan <you@example.com>
2022-05-03 19:04:15 +0300

    email js

--- src/components/pages/Homepage/features/Contact/Contact.jsx
+++ src/components/pages/Homepage/features/Contact/Contact.jsx
@@ -1,16 +1,36 @@
-import React from "react";
+import React,{useRef,useState} from "react";
+import emailjs from "emailjs-com"
 import "./contact.css";
 
 export const Contact = () => {
+    const [done ,setDone] = useState(false)
+    const form = useRef();
+
+	function sendEmail(e){
+		e.preventDefault();
+
+		emailjs.sendForm('service_qq1v59e' , 'template_nqstejk', e.target, '_6YQox5SHEiYQ14wb')
+		  .then((result) => {
+			  console.log(result.text);
+			  setDone(!done)
+		  }, (error) => {
+			  console.log(error.text);
+		  });
+	}
 	return (
 		<div id="contact" className="c-container">
 			<div className="form-wrapper">       
 				<h1 className="form-headline"> ? רוצים עוד מידע </h1>
 				<h4>!השאירו פרטים ונחזור אליכם</h4>
-				<input type="text" placeholder="שם פרטי" />
-				<input type="text" placeholder="שם משפחה" />
-				<input type="text" placeholder="מספר טלפון" />
+				<form ref={form} onSubmit={sendEmail}>
+				<input type="text" name="user_name" placeholder="שם פרטי" />
+				<input type="text" name="last_name" placeholder="שם משפחה" />
+				<input type="tel" name="number" placeholder="מספר טלפון" />
 				<button className="form-btn">שלח</button>
+				{
+                 done && <p className="thanks">הפרטים נשלחו בהצלחה נחזור בהקדם!</p>   
+				}
+				</form>
 			</div>
 		</div>
 	);
--- src/components/pages/Homepage/features/Content/Content.jsx
+++ src/components/pages/Homepage/features/Content/Content.jsx
@@ -13,6 +13,7 @@ export const Content = () => {
   return (
     <div className="c-container">
         <div className="boxes-wrapper">
+            <div className="tighter">
             <div data-aos="fade-up" className="box">
                <i className="bi bi-cash-stack"></i>
                 <h3 className="desc">מחיקת נתונים שליליים</h3>
@@ -29,18 +30,21 @@ export const Content = () => {
                 <p className="paragraph">צוות המומחים שלנו יעזור לכם להתנהל בצורה חכמה יותר על מנת לשפר את נתוני האשראי.</p>
             </div>
         </div>
+            </div>
+        <div className="rate-container">
+        <div className="content-wrapper">
+        <div data-aos="fade-right" className="thinking">
+            <img className="thinking-img" src="https://www.pikpng.com/pngl/b/49-495628_open-thinking-man-cartoon-png-clipart.png" alt="" />
+        </div>
+        <div data-aos="fade-down" className="content">
         <div data-aos="fade-up" className="headline">
             <h1>לא נותנים לכם הלוואה? אל תדאגו, צוות המומחים שלנו יעזור לכם למצוא פתרון.</h1>
         </div>
-        <div className="content-wrapper">
-        <div data-aos="fade-right" className="thinking">
-            <img className="thinking-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBVO7AWHltXKSw3jieishMNF6ZbFWaPRwWPQ&usqp=CAU  " alt="" />
-        </div>
-        <div data-aos="fade-down" className="content">
             <h4 data-aos="fade-down">כאן תוכלו להעריך מהו דירוג האשראי שלכם</h4>
             <div className="rate">
             <img className="graph-img" src="./graph.jpeg" alt="" />
             </div>
+        </div>
         </div>
         </div>
         
